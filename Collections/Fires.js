ad7c2c2c2bdd63fda9248717de8ef5ce6d0bb2f2
MICHAEL WEAVER <mikey.weaver@gmail.com>
2015-07-05 02:12:57 +0800

    Added some sentinel datasets

--- Collections/Fires.js
+++ Collections/Fires.js
@@ -4,7 +4,29 @@
 
   FiresDB = new Mongo.Collection('Fires');
 
-  FiresDB.insert({});
+  FiresDB.insert({
+    "title": "20.425S 122.949E",
+    "category": "modis",
+    "guid": {
+      "isPermaLink": "false",
+      "#text": "tag:45e0591bf2814f413f2d050c3602d349"
+    },
+    "pubDate": "Tue, 06 May 2014 05:30:30 GMT",
+    "description": "          LOCATION: -20.425 122.949<br/>TEMPERATURE: 57.8C<br/>CONFIDENCE: 76%<br/>SATELLITE: aqua<br/>OBS TIME: 2014-05-06T05:30:30Z<br/>GRID: 1km          ",
+    "georss:point": "-20.425 122.949"
+  });
+
+  FiresDB.insert({
+    "title": "20.423S 122.959E",
+    "category": "modis",
+    "guid": {
+      "isPermaLink": "false",
+      "#text": "tag:25f8b9d97ea1b0cbff2498b93bb69377"
+    },
+    "pubDate": "Tue, 06 May 2014 05:30:30 GMT",
+    "description": "          LOCATION: -20.423 122.959<br/>TEMPERATURE: 54.5C<br/>CONFIDENCE: 54%<br/>SATELLITE: aqua<br/>OBS TIME: 2014-05-06T05:30:30Z<br/>GRID: 1km          ",
+    "georss:point": "-20.423 122.959"
+  });
 
 }).call(this);
 
